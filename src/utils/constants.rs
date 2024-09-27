1d58c2b7cb5a51de231bcc4c1d2b89067be9bae9
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-09-27 21:41:59 +0530

    added_constants

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -5,6 +5,18 @@ use std::env;
 lazy_static! {
     pub static ref DATABASE_URL: String = {
         dotenv().ok(); // loading env vars from the file
-        env::var("DATABASE_URL").expect("DATABASE_URL must be set")  
+        env::var("DATABASE_URL").expect("DATABASE_URL must be set")
 };
+pub static ref SMTP_USERNAME: String = {
+        env::var("SMTP_USERNAME").expect("SMTP_USERNAME must be set")
+};
+pub static ref SMTP_PASSWORD: String = {
+        env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD must be set")
+};
+pub static ref SMTP_HOST: String = {
+    env::var("SMTP_HOST").expect("SMTP_HOST must be set")
+};
+pub static ref OTP_SECRET: String = {
+        env::var("OTP_SECRET").expect("OTP_SECRET must be set")
+    };
 }
