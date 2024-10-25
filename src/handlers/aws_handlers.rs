d8c5fb2af4621b70a38bb176a742bc49bc2deac9
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-10-25 21:25:18 +0530

    feat(complete:V.0.0.1):cp

--- src/handlers/aws_handlers.rs
+++ src/handlers/aws_handlers.rs
@@ -3,7 +3,7 @@ use core::str;
 use aws_config::Region;
 use aws_sdk_s3::Client;
 
-pub async fn get_testcase(filename: String) -> Option<String>
+pub async fn get_testcase(filename: &String) -> Option<String>
 // -> Result<GetObjectOutput,SdkError<GetObjectError>>
 {
     let config = aws_config::from_env()
@@ -13,6 +13,43 @@ pub async fn get_testcase(filename: String) -> Option<String>
     let client = Client::new(&config);
     let bucket_name = "affinitys3";
     let key = format!("testcases/{}.txt", filename);
+    println!("{}", key);
+
+    let something = client
+        .get_object()
+        .bucket(bucket_name)
+        .key(key)
+        .send()
+        .await;
+
+    match something {
+        Ok(something) => {
+            let mut body = something.body;
+
+            let mut all_bytes = Vec::new();
+
+            while let Some(bytes) = body.try_next().await.unwrap() {
+                // Convert the byte vector to a string (assuming UTF-8 encoding)
+                all_bytes.extend_from_slice(&bytes);
+            }
+
+            Some(str::from_utf8(&all_bytes).unwrap().to_string())
+        }
+        Err(e) => {
+            eprintln!("Error occurred: {}", e);
+            None
+        }
+    }
+}
+pub async fn get_answer_file(filename: &String) -> Option<String>
+{
+    let config = aws_config::from_env()
+        .region(Region::new("ap-south-1"))
+        .load()
+        .await;
+    let client = Client::new(&config);
+    let bucket_name = "affinitys3";
+    let key = format!("answers/{}.txt", filename);
     println!("{}", key);
 
     let something = client
--- src/handlers/cp_handler.rs
+++ src/handlers/cp_handler.rs
@@ -1,26 +1,45 @@
 use std::{path::Path, process::Stdio};
 
-use axum::extract::Multipart;
+use axum::{extract::Multipart, response::IntoResponse, Json};
+use reqwest::StatusCode;
+use serde_json::Value;
 use tokio::{io::AsyncWriteExt, process::Command};
 
-use crate::{handlers::aws_handlers::get_testcase, utils::code_extensions::{get_language_from_extension, json_value}};
+use crate::{
+    handlers::aws_handlers::{get_answer_file, get_testcase},
+    utils::code_extensions::{get_language_from_extension, json_value},
+};
 
-pub async fn code_handler(mut multipart: Multipart) {
+pub async fn code_handler(mut multipart: Multipart) -> impl IntoResponse {
     while let Some(field) = multipart.next_field().await.unwrap() {
+        // Get the file name and content from the multipart field
         let name = field.file_name().unwrap().to_string();
         let data = field.text().await.unwrap();
 
         let extension = Path::new(&name).extension().and_then(|ext| ext.to_str());
-        let filename_without_ext = Path::new(&name).file_stem().unwrap()  // Get the Option<OsStr>
-        .to_string_lossy() 
-        .to_string(); 
-    
+        let filename_without_ext = Path::new(&name)
+            .file_stem()
+            .unwrap() // Get the Option<OsStr>
+            .to_string_lossy()
+            .to_string();
+
         let language = get_language_from_extension(extension);
 
-        let filename = format!("main.{}", extension.unwrap());
+        // Check for a valid extension
+        let extension = match extension {
+            Some(ext) => ext,
+            None => {
+                eprintln!("No valid extension found for file: {}", name);
+                return Json("Invalid file extension").into_response();
+            }
+        };
+
+        let filename = format!("main.{}", extension);
         let runner_image = format!("glot/{}:latest", language);
 
-        let testcase = get_testcase(filename_without_ext).await.unwrap_or_else(||{"".to_string()});
+        let testcase = get_testcase(&filename_without_ext)
+            .await
+            .unwrap_or_else(|| "".to_string());
 
         // Create a JSON object using the escaped code in "content"
         let json_value = json_value(language, filename, data, testcase);
@@ -46,7 +65,6 @@ pub async fn code_handler(mut multipart: Multipart) {
             .spawn()
             .expect("Failed to start Docker process");
 
-
         // Write the JSON input to the Docker container's stdin
         if let Some(stdin) = docker_process.stdin.as_mut() {
             stdin
@@ -59,10 +77,61 @@ pub async fn code_handler(mut multipart: Multipart) {
         let output = docker_process
             .wait_with_output()
             .await
-            .expect("Failed to read output");
+            .expect("Failed to wait for Docker process");
+            
 
-        // Print stdout and stderr
-        println!("STDOUT: {}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("STDERR: {}", String::from_utf8_lossy(&output.stderr));
+        let stdout_str = String::from_utf8_lossy(&output.stdout);
+        // Parse the JSON from the stdout string
+        let json_value: Value = match serde_json::from_str(&stdout_str) {
+            Ok(value) => value,
+            Err(e) => {
+                eprintln!("Failed to parse JSON: {}", e);
+                return Json("Error in output").into_response(); // Improved error handling
+            }
+        };
+
+        let stdout = json_value.get("stdout").unwrap_or(&Value::Null);
+        let stderr = json_value.get("stderr").unwrap_or(&Value::Null);
+        let error = json_value.get("error").unwrap_or(&Value::Null);
+
+        let stdout_str = serde_json::to_string_pretty(&stdout)
+            .unwrap()
+            .trim_matches('"')
+            .trim()
+            .to_string();
+        let _stderr_str = serde_json::to_string_pretty(&stderr)
+            .unwrap()
+            .trim()
+            .to_string();
+        let _error_str = serde_json::to_string_pretty(&error)
+            .unwrap()
+            .trim()
+            .to_string();
+
+        // Get answer file
+        let answer = get_answer_file(&filename_without_ext).await;
+
+        match answer {
+            Some(expected_answer) => {
+                if expected_answer.trim() == stdout_str {
+                    println!("Test case passed for {}", filename_without_ext);
+                    return Json("Success").into_response();
+                } else {
+                    let fail = format!("Failed:Expected: {}, Got: {}", expected_answer, stdout_str);
+                    return Json(fail).into_response(); // Indicating failure more clearly
+                }
+            }
+            None => {
+                println!("No answer found for {}", filename_without_ext);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json("Database query failed"),
+                )
+                    .into_response();
+            }
+        }
     }
+
+    // Return a default response if no fields were processed
+    Json("No fields processed").into_response()
 }
